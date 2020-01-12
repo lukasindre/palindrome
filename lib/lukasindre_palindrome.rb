ff12e1cf84fbf6a6ba239fcd0106cdd9536c0fb9
Lukas Indre <lukas.indre@upstart.com>
2020-01-12 14:07:21 -0500

    finish work and refactor palindrome method

--- lib/lukasindre_palindrome.rb
+++ lib/lukasindre_palindrome.rb
@@ -1,6 +1,20 @@
 require "lukasindre_palindrome/version"
+class String
 
-module LukasindrePalindrome
-  class Error < StandardError; end
-  # Your code goes here...
+  # Returns true for a palindrome, false otherwise.
+  def palindrome?
+    processed_content == processed_content.reverse
+  end
+
+  # Returns the letters in the string.
+  def letters
+    self.chars.select { |c| c.match(/[a-z]/i) }.join
+  end
+
+  private
+
+    # Returns content for palindrome testing.
+    def processed_content
+      self.scan(/[a-z]/i).join.downcase
+    end
 end
